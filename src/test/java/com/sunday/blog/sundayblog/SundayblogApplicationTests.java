c087d481316d81c2cfff09cafd1cda9457b21827
cyokou <ch_esta@163.com>
2020-04-19 16:28:45 +0800

    template

---
+++ src/test/java/com/sunday/blog/sundayblog/SundayblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunday.blog.sundayblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundayblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
